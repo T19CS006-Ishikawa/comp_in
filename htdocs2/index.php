8e9caf3e81b9ed1a2b367845b32594399a0230c7
T19CS006-Ishikawa <takumi13226@192.168.0.12>
2022-11-14 23:33:46 +0900

    3.3.3

--- htdocs2/index.php
+++ htdocs2/index.php
@@ -3,7 +3,23 @@
     //Composerでインストールしたライブラリを一括読み込み
     require_once__DIR__ . '/vendor/autoload.php';
     
-    //POST メソッドで渡される値を取得、表示
-    $inputString = file_get_contents('php://input');
-    error_log($inputString);
+    //アクセストークンを使いCurlHTTPClientをインスタンス化
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+    
+    //CurlHTTPClientとシークレットを使い、LINE Botをインスタンス化
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+    
+    //LINE Message API がリクエストに付与した署名を取得
+    $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+    
+    //署名が正当かチェック。正当であればリクエストをパースし配列へ
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+    
+    foreach ($events as $event){
+        //テキストを返信
+        $bot->replyText($event -> getReplyToken(),'TextMessage');
+    }
+    
+    
+    
 ?>
