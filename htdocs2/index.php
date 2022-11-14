706650313820676452aac590fe5671ea88dae968
T19CS006-Ishikawa <takumi13226@192.168.0.12>
2022-11-14 20:28:05 +0900

    delete vendor and edit index.php

--- htdocs2/index.php
+++ htdocs2/index.php
@@ -1,4 +1,9 @@
 <?php
 
-    echo "This is from [htdocs] repository";
+    //Composerでインストールしたライブラリを一括読み込み
+    require_once__DIR__ . '/vendor/autoload.php';
+    
+    //POST メソッドで渡される値を取得、表示
+    $inputString = file_get_contents('php://input');
+    error_log($inputString);
 ?>
