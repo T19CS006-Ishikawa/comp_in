f7687b2a594b9af583e338998535068b7ec60168
T19CS006-Ishikawa <takumi13226@172.24.91.181>
2022-11-15 11:22:04 +0900

    改行を追加

--- htdocs2/index.php
+++ htdocs2/index.php
@@ -38,8 +38,7 @@ if($event->type == 'message') {
         } else if($messageData->text == 'マルティン') {
             $replyText = 'ウーデゴール';
         } else if($messageData->text == 'アーセナル') {
-            $replyText = 'ヘッドコーチ：ミケル・アルテタ';
-            $replyText = 'キャプテン：ミケル・アルテタ';
+            $replyText = 'ヘッドコーチ：ミケル・アルテタ＜br＞キャプテン：マルティン・ウーデゴール';
         }else {
             $replyText = $messageData->text;
         }
