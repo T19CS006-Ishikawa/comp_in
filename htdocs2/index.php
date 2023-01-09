4cde3f3c5806de11f9792796645575413c8eed5a
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-10 03:40:10 +0900

    define delote

--- htdocs2/index.php
+++ htdocs2/index.php
@@ -1,9 +1,9 @@
 <?php
-
+/*
 define('ACCESSTOKEN', 'DzNOTsY/Ht98S9QxPPUNcjN5m85UvSY1lEpZyfyLN+RFcuUs2laRz664s33dV/DYK+Q1b4COWuJTxhs/NBc0KcTQG4Fe52Gz+rsdAWSLbFQ8m8FAHoP/gU7lwCg5vZlggPAQOZAkKOBa7iDDAc+xNAdB04t89/1O/w1cDnyilFU=');
 define('SECRET', '9dd5b0db9baa14fcfb44558627c6fbeb');
 define('USERID', 'U9efd3497669302c628518420a60ebe92');
-
+*/
 /*
  
     //CurlHTTPClientとシークレットを使い、LINE Botをインスタンス化
