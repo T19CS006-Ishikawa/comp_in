483788a5895a0fcb487566cd001fb56524ec8ae0
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-10 03:47:37 +0900

    comment out

--- htdocs2/index.php
+++ htdocs2/index.php
@@ -1,9 +1,9 @@
 <?php
-/*
+
 define('ACCESSTOKEN', 'DzNOTsY/Ht98S9QxPPUNcjN5m85UvSY1lEpZyfyLN+RFcuUs2laRz664s33dV/DYK+Q1b4COWuJTxhs/NBc0KcTQG4Fe52Gz+rsdAWSLbFQ8m8FAHoP/gU7lwCg5vZlggPAQOZAkKOBa7iDDAc+xNAdB04t89/1O/w1cDnyilFU=');
 define('SECRET', '9dd5b0db9baa14fcfb44558627c6fbeb');
 define('USERID', 'U9efd3497669302c628518420a60ebe92');
-*/
+
 /*
  
     //CurlHTTPClientとシークレットを使い、LINE Botをインスタンス化
@@ -22,7 +22,6 @@ define('USERID', 'U9efd3497669302c628518420a60ebe92');
     
    */
 
-/*-------------
 //Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 $input = file_get_contents('php://input');
@@ -32,8 +31,8 @@ $json = json_decode($input);
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(ACCESSTOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret'=>SECRET]);
 
----------------
-*/
+
+
 //$testData = file_get_contents($filename); 
 $test = "テストメッセージです";
 
@@ -68,8 +67,8 @@ if($event->type == 'message') {
 */
 //＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿//
 
-///$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($test);
-///$bot->pushMessage(USERID, $textMessageBuilder);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($test);
+$bot->pushMessage(USERID, $textMessageBuilder);
 echo $test;
 return;
 ?>
