e80a0b6e7c3c8d86905adc715aaaa7dbd706b27f
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-12 11:07:52 +0900

    com change

---
+++ htdocs2/getCSV.php
@@ -0,0 +1,50 @@
+<?php 
+
+//＿＿＿＿htdocs2＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿//
+
+$dlpath = 'https://file-upload-app.herokuapp.com/upfile';
+$path = 'https://app-for-lms.herokuapp.com/csvData';
+
+
+$fp = "list.txt";
+$read = file_get_contents($fp);
+//$list = file($read);
+$list = explode("|",$read );
+var_dump($list);
+
+//list[0] = csvファイル名になる
+//$csvname = '/'.$list[0];
+
+//$dlroot = $dlpath.$csvname;
+//$root = $path.$csvname;
+
+
+echo "ダウンロード元：".$dlroot;
+echo "<br>";
+echo "ダウンロード先：".$root;
+echo "<br>";
+
+
+
+
+$data = file_get_contents($dlroot);
+echo $data;
+echo "<br>";
+
+file_put_contents('./csvData',$data); //ファイルの保存先
+
+
+
+//check
+$filename = "csvData/課題データ2.csv";
+
+if (file_exists($filename)) {
+    echo "$filename が存在します";
+} else {
+    echo "$filename は存在しません";
+}
+/*＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿*/
+
+
+?>
+
--- htdocs2/pushMessage.php
+++ htdocs2/pushMessage.php
@@ -14,8 +14,6 @@ define('SECRET', '9dd5b0db9baa14fcfb44558627c6fbeb');
 define('USERID', 'U9efd3497669302c628518420a60ebe92');
 
 
-
-
 //Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 $input = file_get_contents('php://input');
@@ -26,16 +24,15 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(ACCESSTOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret'=>SECRET]);
 
 
-
 //$testData = file_get_contents($filename);
-$test = "テストメッセージです";
+$message = "今日は１月１２日です";
 
 //$event = $json->events[0];
 
 
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($test);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 $bot->pushMessage(USERID, $textMessageBuilder);
-echo SECRET;
+echo $test;
 return;
 ?>
