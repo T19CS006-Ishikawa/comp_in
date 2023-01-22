9b2e0ddca3f19e6659f8e9f8f7023cdfb0023684
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-23 05:28:15 +0900

    back

--- htdocs2/index.php
+++ htdocs2/index.php
@@ -5,7 +5,7 @@
 <head>
 <meta http-equiv="Content-Type" content = "text/html;charset=UTF-8">
 </head>
-<form action = "./multiCast.php"	method="POST">
+<form action = "./pushMessage.php"	method="POST">
 <input type = "submit" value ="メッセージ送信">
 </form>
 </body>
