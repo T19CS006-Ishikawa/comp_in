137a8dff996acf6cb737f7d911c00781708872d5
T19CS006-Ishikawa <takumi13226@192.168.11.9>
2023-01-25 03:15:37 +0900

    lo

--- htdocs2/multiCast.php
+++ htdocs2/multiCast.php
@@ -8,11 +8,12 @@
 </form>
 </body>
 </html>
+
 <?php
+
 define('ACCESSTOKEN', 'DzNOTsY/Ht98S9QxPPUNcjN5m85UvSY1lEpZyfyLN+RFcuUs2laRz664s33dV/DYK+Q1b4COWuJTxhs/NBc0KcTQG4Fe52Gz+rsdAWSLbFQ8m8FAHoP/gU7lwCg5vZlggPAQOZAkKOBa7iDDAc+xNAdB04t89/1O/w1cDnyilFU=');
 define('SECRET', '9dd5b0db9baa14fcfb44558627c6fbeb');
 //define('USERID', 'U9efd3497669302c628518420a60ebe92');
-
 
 //Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
@@ -22,11 +23,13 @@ $json_object = json_decode($json_string);
 $event_type     = $json_object->{"events"}[0]->{"type"};
 $userId         = $json_object->{"events"}[0]->{"source"}->{"userId"};
 
-
 if($event_type === "follow" || $event_type === "unfollow"){
-    file_put_contents("follow.log", $event_type . " " .$userId . "\n", FILE_APPEND);
+    file_put_contents("follow.log",$userId . ",", FILE_APPEND);
     exit;
 }
+$ids = file_get_contents("file.log");
+$user_ids = explode(',', $ids);
+$trush = array_pop($user_ids);
 
 //アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(ACCESSTOKEN);
@@ -40,23 +43,9 @@ echo $data;
 
 $message = $data;
 
-$replyToken     = $json_object->{"events"}[0]->{"replyToken"};
-//$message_text   = $json_object->{"events"}[0]->{"message"}->{"text"};
-//$message_type   = $json_object->{"events"}[0]->{"message"}->{"type"};
-
-$response_format_text = array( "type" => 'text', "text" => $message );
-$post_data = array( "replyToken" => $replyToken, "messages" => [$response_format_text] );
-
-$ch = curl_init("https://api.line.me/v2/bot/message/reply");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $post_data, JSON_UNESCAPED_UNICODE ));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . ACCESSTOKEN
-));
-curl_exec($ch);
-curl_close($ch);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+ //$bot->pushMessage(USERID, $textMessageBuilder);
+$bot->multicast($user_ids, $textMessageBuilder);
+echo $test;
 return;
 ?>
